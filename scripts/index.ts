a21f8e7fc43d28ca53ccf08e0687d5859e341c66
Roman Melnyk <rmelnyk@thepeoplegroup.com>
2019-12-25 00:58:09 +0100

    Draw buttons on playground

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,2 +1,14 @@
-const message = 'TS works';
-console.info(message);
+function init() {
+  const playgroundEl: HTMLDivElement = document.querySelector('.playground');
+
+  const fieldSize = 9;
+  for (let x = 0; x < fieldSize; x++) {
+    for (let y = 0; y < fieldSize; y++) {
+      const buttonEl = document.createElement('div');
+      buttonEl.className = 'button';
+      playgroundEl.appendChild(buttonEl);
+    }
+  }
+}
+
+window.addEventListener('load', init);
