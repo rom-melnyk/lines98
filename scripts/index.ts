641f13879d2e91011c30eb92bb79a3619d6d2070
Roman Melnyk <rmelnyk@thepeoplegroup.com>
2019-12-25 15:22:28 +0100

    Playground styling refactoring

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,11 +1,12 @@
 function init() {
   const playgroundEl: HTMLDivElement = document.querySelector('.playground');
+  playgroundEl.classList.add('palette-1');
 
   const fieldSize = 9;
   for (let x = 0; x < fieldSize; x++) {
     for (let y = 0; y < fieldSize; y++) {
       const buttonEl = document.createElement('div');
-      buttonEl.className = 'button';
+      buttonEl.className = 'cell';
       playgroundEl.appendChild(buttonEl);
     }
   }
