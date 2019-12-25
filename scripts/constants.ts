9edafb516a131c3d65e1932ba549f6b284a53c35
Roman Melnyk <rmelnyk@thepeoplegroup.com>
2019-12-25 21:28:05 +0100

    Add "non-zero" comments to constants

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -2,10 +2,16 @@ export const playgroundSize = 9;
 export const lineSize = 4;
 export const ballsPerIntention = 3;
 
-/** @see ../styles/playground/_palettes.scss */
+/**
+ * @see ../styles/playground/_palettes.scss
+ * Must not contain 0; {@see ./playground.ts#Plaground}.
+ */
 export const palettes = [1];
 
-/** @see ../styles/playground/_palettes.scss */
+/**
+ * @see ../styles/playground/_palettes.scss
+ * Must not contain 0; {@see ./cell.ts#Cell}.
+ */
 export const colors = [1, 2, 3, 4, 5];
 
 export class GameOver extends Error{}
