af73feacecec3072c3a961831bdcdd83812c27d9
Roman Melnyk <rmelnyk@thepeoplegroup.com>
2019-12-27 13:21:01 +0100

    Add `serialize/deserialize` to Cell

--- scripts/cell.ts
+++ scripts/cell.ts
@@ -1,6 +1,37 @@
+import * as constants from './constants';
+
 type CellProperties = 'ball' | 'intention' | 'trace' | 'selected';
 
 export class Cell {
+  public static fromSerialized(serialized: string): Cell {
+    const [coordsMarker, ballMarker, intentionMarker] = serialized.split('|');
+
+    const coords = coordsMarker.split(':');
+    const x = Number(coords[0]);
+    const y = Number(coords[1]);
+    if (
+      isNaN(x) || x < 0 || x >= constants.playgroundSize
+      || isNaN(y) || y < 0 || y >= constants.playgroundSize
+    ) {
+      throw new Error(`Cannot parse coordinates "${coordsMarker}"`);
+    }
+
+    const ball = ballMarker ? Number(ballMarker) : null;
+    if (ball !== null && constants.colors.indexOf(ball) === -1) {
+      throw new Error(`Cannot parse ball "${ballMarker}"`);
+    }
+
+    const intention = intentionMarker ? Number(intentionMarker) : null;
+    if (intention !== null && constants.colors.indexOf(intention) === -1) {
+      throw new Error(`Cannot parse intention "${intentionMarker}"`);
+    }
+
+    const cell = new Cell(x, y);
+    cell.set('ball', ball);
+    cell.set('intention', intention);
+    return cell;
+  }
+
   private readonly htmlElement: HTMLDivElement;
   private properties: { [key in CellProperties]: number } = {
     ball: null,
@@ -33,5 +64,16 @@ export class Cell {
     }
 
     this.properties[property] = value;
+  }
+
+  public serializeCoords() {
+    return `${this.x}:${this.y}`;
+  }
+
+  public serialize() {
+    const coords = this.serializeCoords();
+    const ball = this.get('ball') ? this.get('ball') : '';
+    const intention = this.get('intention') ? this.get('intention') : '';
+    return `${coords}|${ball}|${intention}`;
   }
 }
