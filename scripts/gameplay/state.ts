5f5972c97ae59becc3c397f0b6229f720ae6df6c
Roman Melnyk <rmelnyk@thepeoplegroup.com>
2019-12-28 17:50:27 +0100

    Introduce State.lastBallMove

--- scripts/gameplay/state.ts
+++ scripts/gameplay/state.ts
@@ -6,6 +6,7 @@ export class State {
   public lastSettled: Cell[] = [];
   public lastWipedCells: Cell[] = [];
   public lastWipedColor: number = null;
+  public lastBallMove: [Cell, Cell] /* from, to */ = null;
   public score = 0;
 
   constructor() {}
