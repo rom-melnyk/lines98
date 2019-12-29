5e21db98967f02d2ec1a7d6e40e72fafa8850779
Roman Melnyk <rmelnyk@thepeoplegroup.com>
2019-12-29 22:34:09 +0100

    Optimize score calculation

--- scripts/gameplay/runtime.ts
+++ scripts/gameplay/runtime.ts
@@ -16,16 +16,18 @@ export class Runtime {
   }
 
   public updateScore(cellsWiped: number) {
-    const increment = 1;
-    const sign = Math.sign(cellsWiped);
-    cellsWiped = Math.abs(cellsWiped);
-    while (cellsWiped > 0) {
-      const perHopIncrement = cellsWiped > constants.lineSize
-        ? (cellsWiped - constants.lineSize) * increment
-        : increment;
-      this.score += sign * perHopIncrement;
-      cellsWiped--;
-    }
+    // For each ball of minimal line the `cost` is added to the score.
+    // For each ball over minimal line size the cost is increased by one:
+    //  - line of 1..5 balls   = 5 * cost,
+    //  - line of 6 balls      = 5 * cost + 2 * cost,
+    //  - line of 7 balls      = 5 * cost + 2 * cost + 3 * cost,
+    //  - line of 8 balls      = 5 * cost + 2 * cost + 3 * cost + 4 * cost,
+    // ...and so on. Think "arithmetical progression".
+    const cost = 1;
+    const oversize = Math.abs(cellsWiped) - constants.lineSize;
+    this.score += cellsWiped * cost;
+    // Sum of arithmetical progression {from = 2; increment = 1; steps = oversize}.
+    this.score += Math.sign(cellsWiped) * cost * (2 + oversize + 1) * oversize / 2;
 
     let score = this.score.toString(10);
     score = Array(5 - score.length).fill('0').join('') + score;
